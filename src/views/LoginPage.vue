24abcd73d82d5504bc866ebdf494f3e82e4a9b09
cre4tive97 <harryborrison5148@gmail.com>
2021-12-03 21:30:39 +0900

    clone tree

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>LoginPage</div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LoginPage',
+
+  setup() {
+    //
+  },
+});
+</script>
---
+++ src/views/SignupPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>SignUp</div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SignupPage',
+
+  setup() {
+    //
+  },
+});
+</script>
